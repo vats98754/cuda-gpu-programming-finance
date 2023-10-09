89dae0a98e25cd275d67b50d179f49c0bd468661
Anvay Vats <73335206+vats98754@users.noreply.github.com>
2023-10-08 22:56:57 -0400

    Add Black Scholes Visualization

--- src/black_scholes_visualization.cu
+++ src/black_scholes_visualization.cu
@@ -1,28 +1,50 @@
-// black_scholes_visualization.cu
-
 #include <iostream>
 #include <cmath>
 #include <cuda_runtime.h>
+#include <curand_kernel.h>
 #include <GL/glut.h> // OpenGL utility library
 
-__global__ void blackScholesKernel(float *d_result, float S, float K, float T, float r, float sigma, int numOptions) {
-    // Placeholder for Black-Scholes model implementation
-    // For now, just producing some dummy data
+// Constants for Black-Scholes computation
+const int numOptions = 1000;
+const float S = 100.0f;  // Stock price
+const float K = 100.0f;  // Strike price
+const float T = 1.0f;    // Time to maturity
+const float r = 0.05f;   // Risk-free rate
+const float sigma = 0.2f; // Volatility
+
+// CUDA kernel for Black-Scholes computation
+__global__ void blackScholesKernel(float *d_callResult, float *d_putResult) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < numOptions) {
-        d_result[tid] = sinf(tid * 0.1f); // Dummy data
-    }
+    if (tid >= numOptions) return;
+
+    float d1 = (logf(S/K) + (r + 0.5f * sigma * sigma) * T) / (sigma * sqrtf(T));
+    float d2 = d1 - sigma * sqrtf(T);
+    float call_option_price = S * normcdf(d1) - K * expf(-r * T) * normcdf(d2);
+    float put_option_price = K * expf(-r * T) * normcdf(-d2) - S * normcdf(-d1);
+    
+    d_callResult[tid] = call_option_price;
+    d_putResult[tid] = put_option_price;
 }
 
+// Norm CDF function (using Abramowitz & Stegun approximation)
+__device__ float normcdf(float x) {
+    float k = 1.0f / (1.0f + 0.2316419f * fabsf(x));
+    float cnd = k * (1.17302f + k * (-1.3026539f + k * (0.482967f + k * (-0.226380459f + k * 0.02698f))));
+    if (x < 0)
+        cnd = 1.0f - cnd;
+    return cnd;
+}
+
+// OpenGL display function
 void displayFunction() {
     glClear(GL_COLOR_BUFFER_BIT);
     
     // Draw the results of the Black-Scholes model
-    // This is just a placeholder. In a full implementation, you would retrieve the data from the GPU
-    // and use OpenGL calls to visualize it.
     glBegin(GL_LINE_STRIP);
-    for (int i = 0; i < 100; ++i) {
-        glVertex2f(i * 0.01f - 0.5f, sinf(i * 0.1f) * 0.5f);
+    for (int i = 0; i < numOptions; ++i) {
+        float x = (i * 2.0f / numOptions) - 1.0f;
+        float y = (sinf(i * 0.1f) * 0.5f) - 0.5f; // Dummy visualization for now
+        glVertex2f(x, y);
     }
     glEnd();
 
@@ -31,7 +53,10 @@ void displayFunction() {
 
 int main() {
     // Placeholder for CUDA setup and invocation of the kernel
-    // ...
+    float *d_callResult, *d_putResult;
+    cudaMalloc(&d_callResult, numOptions * sizeof(float));
+    cudaMalloc(&d_putResult, numOptions * sizeof(float));
+    blackScholesKernel<<<(numOptions + 255) / 256, 256>>>(d_callResult, d_putResult);
 
     // Setup for OpenGL visualization
     int argc = 1;
@@ -42,6 +67,10 @@ int main() {
     glutCreateWindow("Black-Scholes Visualization");
     glutDisplayFunc(displayFunction);
     glutMainLoop();
+
+    // Free CUDA memory
+    cudaFree(d_callResult);
+    cudaFree(d_putResult);
 
     return 0;
 }
